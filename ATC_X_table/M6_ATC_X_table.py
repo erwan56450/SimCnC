d50293ca904d2a25ca5a042cbd3fd54415b764be
Erwan <130402510+erwan56450@users.noreply.github.com>
2023-04-22 22:49:08 +0000

    -

--- ATC_X_table/M6_ATC_X_table.py
+++ ATC_X_table/M6_ATC_X_table.py
@@ -1,62 +1,10 @@
-# Copier/Coller ce code Dans SimCNC> macroEditor> file> open> M6 
-# Copy/past in SimCNC> macroEditor> file> open> M6
+# Copier/Coller ce code Dans SimCNC> macroEditor> file> open> M6.py 
+# Copy/past in SimCNC> macroEditor> file> open> M6.py
+
+# Le fichier Configmachine.py doit etre placer dans le meme répèrtoir que celui ci M6.py
 
 
-# Code python pour changer d'outil sur fraise ATC automatiqueement et le mesurer si sa valeur dans la table d'outils est =0 sinon ne le mesure pas.
-# (Python code to automatically change the tool on an ATC router and measure it if its value in the tool table is = 0, otherwise don't measure it)
 
-# pour le logiciel SIMCNC avec un controler Csmio-s ip7
-# Erwan Le Foll 16/04/2022    https://youtube.com/@erwan3953
-
-# Le Homming de ce code ce fait en haut a droit de votre table au valeur home=Y0,X0,Z0. La zone de travail est donc en valeurs negatives.(peux ce modifier)
-# (The homing in this code is done in the top right of your table with home values = Y0, X0, Z0. The working area is therefore in negative values. (can be modified))
-
-#-----------------------------------------------------------
-# INfos sur la machine (Machine informations)
-#-----------------------------------------------------------
-
-# vitesses (speed)
-Z_down_final_speed = 2000           # Vitesse de Z d'aproche finale lente (slow final approach speed of Z)
-Z_down_fast_speed = 5000            # vitesse de Z d'aproche rapide (fast approach speed of Z)
-Z_up_speed = 10000                  # viteese de lever du Z (speed to lift Z)
-YX_speed = 50000                    # Vitesse de l'axe Y et X (speed of Y and X axis)
-
-#positions
-Y_position_first_tool = -60         # position Y du trou (Y position of the hole)
-Y_position_safe_zone = -210         # zone ou les outils peuvent circuler sur l'axe X sans toucher les autres porte outils (zone where tools can move on the X axis without touching each other)
-X_position_first_tool = -296        # position du premier outil (position of the first tool)
-Z_position_tools = -206.5             # emplacement Z ou l'outil est libéré (location where the tool is released)
-Z_position_approach = -170          # emplacement Z ou il faut commencer a ralentir et declanche valve_clean_cone ou valve_blower  (location where it is necessary to start slowing down and trigger the air conne cleaner)
-X_distance_between_tools = -150     # distance entre les support d'outils sur la table (distance between tool holders)
-
-# numeros d'entrée/sorties 
-ToolCount = 11                      # Nombre max. d'outils sur la table premier outil =1 (Maximum number of tools on the table, first tool=1)
-check_tool_in_spindel = 24          # Numéro de l'entrée numérique qui gère le détecteur d'outil inséré, None=desactivé (Digital input number managing the tool detection sensor)
-check_clamp_status = 25             # Numéro de l'entrée numérique qui gère le détecteur d'ouverture de la griffe du conne , None=desactivé (Digital input number managing the cone clamp open sensor)
-valve_collet = 13                   # Numéro de la sortie numérique qui gère la valve pour le changement d'outil (Digital output number managing the valve for tool change)
-valve_clean_cone = 15               # Numéro de la sortie numérique qui gère la valve pour le nettoyage du cone du porte outil (Digital output number managing the valve for tool holder cone cleaning)
-valve_blower = 12                   # Numéro de la sortie numérique qui gère la valve de la soufflette (Digital output number managing the valve for the blower)
-blowing_time = 0.5                  # temps en seconde du coup de soufflette a la dépose d'un outil ou a la mesure (Time in seconds of the blower at the tool drop or measurement).
-time_spindle_stop = 8               # temps en seconde  de l'arrete de votre broche avec l'outil le plus lourd (time in seconds for the stop of your spindel with the heaviest tool)
-
-#-----------------------------------------------------------
-# Infos sur le Contacteur de palpage (probing infos)
-#-----------------------------------------------------------
-
-do_i_have_prob = True               # True = mesure d'outil activée. False = mesure d'outil desactivée
-every_time_get_measure = True       # true = mesure a tous les coups, False = mesure que si la table d'outil est a zero 
-probeStartAbsPos = {'X_probe': -108, 'Y_probe': -60, 'Z_probe': -80} # Coordonnées de placement au dessus du prob [X_probe, Y_probe, Z_probe] votre outil le plus long doit passer avec ce Z! (Placement coordinates above the probe [X_probe, Y_probe, Z_probe] Your longest tool must pass with this Z!)
-probeIndex = 0                      # corespond a l'entrée que vous avez configuré dans les settings de simcnc (settings->Modules->IO Signals  : 0,1,2 ou 3 possible)
-zEndPosition = -190                 # l'axe z ne descendra pas plus loint! (The Z-axis will not go down any further!)
-refToolProbePos = -143.67           # hauteur a la quelle votre outil de reférénce touche le prob, (si votre outil de référence touche a Z-100mm et que vous indiquez - 100mm ici, alors le décalage enregistré sera de 0mm) (Height at which your reference tool touches the probe (if your reference tool touches at Z-100mm and you indicate - 100mm here, then it will be referenced to 0mm))
-fastProbeVel = 700                  # vitesse de la premiere mesure, rapide (units/min) (Speed of the first, fast measurement (units/min))
-slowProbeVel = 250                  # vitesse du deuxieme mesure, lente (units/min) (Speed of the second, slow measurement (units/min))
-goUpDist = 6                        # remontée en mm de Z entre les deux mesures (Z-axis up travel in mm between the two measurements)
-fineProbingDelay = 0.2              # temps en secondes entre les deux mesures (Time in seconds between the two measurements)
-checkFineProbingDiff = False        # ne pas changer (Do not change)
-fineProbeMaxAllowedDiff = 0.1       # tolerence entre les deux mesures (tolerance between the two measurements)
-moveX = True                        # ne pas changer (Do not change)
-moveY = True                        # ne pas changer (Do not change)
 
 X = 0                               # donne a noms a l'axe quand getposition est utilisé
 Y = 1                               
@@ -67,6 +15,12 @@ C = 5
 
 import time                         # importe le temps pour la fonction time.sleep (import time for the function time.sleep)
 import sys                          # pour utiliser la fonction sys.exit() (to use the sys.exit() function)
+
+try:
+    import ConfigMachine # Import les variable/infos du fichier ConfigMachine.py 
+except ImportError:
+    msg.info("File ConfigMachine.py not found. !")
+    sys.exit(1) 
 
 #-----------------------------------------------------------
 # Importe le tradution du fichier multilingual.py a placer dans le meme répèretoir que M6
