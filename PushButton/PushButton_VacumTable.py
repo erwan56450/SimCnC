57e16e85472bdc4ddc324cf627004b2c2107789e
Erwan <130402510+erwan56450@users.noreply.github.com>
2023-05-14 11:21:27 +0000

    -

--- PushButton/PushButton_VacumTable.py
+++ PushButton/PushButton_VacumTable.py
@@ -2,12 +2,15 @@
 # set the corect output number here
 OutputToControl = 0
 
-#link this script to a push button in simcnc
+# link this script to a push button in simcnc
+# you must copy/past this file where simcnc open the windows when you clique "ouput clicked> run script "
 
 
 import sys
 
-mod_IP = d.getModule(ModuleType.IP, 0)
+
+#call csmio ips
+mod_IP = d.getModule(ModuleType.IP, 0) 
 
 if mod_IP.getDigitalIO(IOPortDir.OutputPort, OutputToControl) == DIOPinVal.PinReset:
     print("Aspiration ON")
