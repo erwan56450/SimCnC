7f851f7665d05abfd4ed80b0078c7f661f0cf7a0
erwan <erwan@fridu.net>
2023-08-14 22:30:02 +0200

    add bouton

--- Manual_TC/M6.py
+++ Manual_TC/M6.py
@@ -8,11 +8,11 @@
 # The author shall not be held responsible for any injury, damage, or loss resulting from the use of this code.
 # By using this code, you agree to assume all responsibility and risk associated with the use of the code.
 
-# If like me you have bought a Simcnc card but your Atc is not ready, you will need to change the tool manually.
-# Replace the M6.py 
+# If like me you have bought a Simcnc card but your change tool rack is not ready , you will need to change the tool manually.
+# Replace the M6.py in the simcnc folder by thise one.
 
 # where you want the spindel for the change of tools
-Y_pos_chang_tool = -1500
+Y_pos_chang_tool = -1400
 X_pos_chang_tool = -1000
 Z_pos_chang_tool = 0
 
@@ -22,7 +22,7 @@ speed = 2000
 # other
 mesure_the_tool = True      #if probing.py is configure you can mesure the tool after clique OK 
 
-# name for d.getposition axis
+# name axis when d.getposition respond
 X = 0
 Y = 1
 Z = 2
@@ -37,7 +37,29 @@ new_tool_length = d.getToolLength(new_tool)
 # Get the machine's position and name it "position".
 position = d.getPosition(CoordMode.Machine)
 
+# Import the library for message boxes
+import tkinter as tk
+from tkinter import messagebox
 
+# Create a message box with custom-sized buttons and window
+def show_message_box():
+    root = tk.Tk()  # Create a temporary root window
+    root.withdraw()  # Hide the root window
+
+    # Adjust the size of the buttons and window
+    root.option_add('*TButton*padding', 10)  # Adjust button padding
+    root.option_add('*TButton*font', ('Helvetica', 12))  # Adjust button font size
+    root.option_add('*Dialog.msg.font', ('Helvetica', 14))  # Adjust message font size
+
+    result = messagebox.askquestion(
+        "Info",
+        "gcode ask tool = " + str(new_tool) + "\nContinue without measuring?",
+        icon='info',
+        type=messagebox.YESNO
+    )
+
+    if result == 'no':  # If the "Measure tool" button is clicked
+        exec(open('probing.py').read())
 
 ########################################################
 # macro start
@@ -52,19 +74,12 @@ position[Y] = Y_pos_chang_tool
 position[X] = X_pos_chang_tool
 d.moveToPosition(CoordMode.Machine, position, speed)
 
-# window popup 
-msg.info( "inséré l'outil  = " + str(new_tool), "Info" )
-
-#d.stopTrajectory()
-#d.setGCodeNextLine()
+#call the message box display function
+show_message_box()
 
 # Export the new tool information to SimCNC.
 d.setToolLength (new_tool,new_tool_length)
 d.setToolOffsetNumber(new_tool)
 d.setSpindleToolNumber(new_tool)
-
-if mesure_the_tool == True
-    exec(open('probing.py').read())
-
 
 print ("Manuel change tool finish")
